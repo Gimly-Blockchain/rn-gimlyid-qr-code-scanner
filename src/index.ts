b978dadc9ee1b2e1da86e44cd70accad9ca6a5a1
Sander Postma <spostma@sphereon.com>
2021-09-20 17:06:30 +0200

    Added exports for QRType, QRMode, QRContent

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { memo } from 'react';
 
 import { GimlyIDQRCodeScanner } from './GimlyIDQRCodeScanner/index';
 
+export { QRType, QRMode, QRContent } from 'gimlyid-qr-code';
 export default memo(GimlyIDQRCodeScanner);
