5e835af454f00280b426ea8b091600681106d0ec
Sander Postma <spostma@sphereon.com>
2021-09-24 17:16:01 +0200

    Added @sphereon scope

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { memo } from 'react';
 
 import { GimlyIDQRCodeScanner } from './GimlyIDQRCodeScanner/index';
 
-export { QRType, QRMode, QRContent } from 'gimlyid-qr-code';
+export { QRType, QRMode, QRContent } from '@sphereon/gimlyid-qr-code';
 export default memo(GimlyIDQRCodeScanner);
